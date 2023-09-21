7b98de6e08ad943ae712ce65a0cd53fa64e0f9d4
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-21 22:42:36 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (num1, num2) => {
+    num1 = 2000;
+    num2 = 100;
+    return num1 / num2;
+}
+
+console.log(divide());
+
+const square = (num3) => {
+    return (num3 * num3)
+}
+console.log(square(4));
+
+const add = (num4, num5) => {
+    return (num4 + num5)
+}
+console.log(add(3, 7));
